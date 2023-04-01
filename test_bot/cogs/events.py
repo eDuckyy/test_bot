2fe33703b31a1687c2f95ae1cf17ce1101b3ff75
eDuckyy <92658584+eDuckyy@users.noreply.github.com>
2023-04-01 22:44:23 +1100

    Update events.py

--- test_bot/cogs/events.py
+++ test_bot/cogs/events.py
@@ -1,32 +1,33 @@
-# SPDX-License-Identifier: MIT
-
 from disnake.ext import commands
+import discord
+import itertools
+import datetime
 
+def iter_all_mentions(message):
+    return itertools.chain(message.mentions, message.channel_mentions, message.role_mentions)
 
-class EventListeners(commands.Cog):
-    def __init__(self, bot) -> None:
-        self.bot: commands.Bot = bot
-
-    @commands.Cog.listener()
-    async def on_guild_scheduled_event_create(self, event) -> None:
-        print("Scheduled event create", repr(event), sep="\n", end="\n\n")
-
-    @commands.Cog.listener()
-    async def on_guild_scheduled_event_update(self, before, after) -> None:
-        print("Scheduled event update", repr(before), repr(after), sep="\n", end="\n\n")
+class AntiPing(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.Cog.listener()
-    async def on_guild_scheduled_event_delete(self, event) -> None:
-        print("Scheduled event delete", repr(event), sep="\n", end="\n\n")
+    async def on_message(message):
+        for i, mentioned in enumerate(iter_all_mentions(message)):
+            allowed_roles = {1089165585214087270, 1010144986806878290, 1000701304110338170, 1032993373470085160, 1070260250709590057}
+            if mentioned.id == 746966989347618858 and not any(role.id in allowed_roles for role in message.author.roles):
+                DateTime = datetime.datetime.now()
+                embed = discord.Embed(
+                    title="Please refrain from pinging the Founder",
+                    description="""
+                    We do not allow you to ping our Founder at this time. For support, please send a Direct Message to @Pleasant Support. Multiple pings to the Founder may result in further moderation action taken.
 
-    @commands.Cog.listener()
-    async def on_guild_scheduled_event_subscribe(self, event, user) -> None:
-        print("Scheduled event subscribe", event, user, sep="\n", end="\n\n")
+If you are replying to a message sent by the Founder, please follow the instructions provided in the video below on how to disable pinging the original author.
+                    """,
+                    color=0xffe1b4
+                )
+                embed.set_footer(text=f"Pleasant Support • Pleasant Corporation • Today at {DateTime.strftime('%x %H:%M')}")
+                embed.set_image(url="https://media.tenor.com/ddg6ge2H_T0AAAAd/discord-reply.gif")
+                await message.channel.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_guild_scheduled_event_unsubscribe(self, event, user) -> None:
-        print("Scheduled event unsubscribe", event, user, sep="\n", end="\n\n")
-
-
-def setup(bot) -> None:
-    bot.add_cog(EventListeners(bot))
+async def setup(bot):
+    bot.add_cog(AntiPing(bot))
